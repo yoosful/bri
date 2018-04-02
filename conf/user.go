f7e7d8ecda3e61da7a9f6f9853778ec192d8f10b
Seonghyun Park <pseohy@gmail.com>
2018-04-02 11:55:19 +0900

    user status (used time and price)

--- conf/user.go
+++ conf/user.go
@@ -3,7 +3,9 @@
 package conf
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
 )
 
 // User represents a registered user
@@ -19,10 +21,27 @@ type User struct {
 
 	// User usage summary
 	Usage map[string]float64 `json:"usage"`
+
+	// Accessible devices
+	Accessible []string `json:"accessible"`
 }
 
 type Users struct {
 	Data []User
+}
+
+// UserStatusEntry represents information of a device used by a user
+type UserStatusEntry struct {
+	Dtype string  `json:"dtype"`
+	Did   string  `json:"did"`
+	Rate  float64 `json:"rate"`
+	Used  float64 `json:"used"`
+}
+
+// UserStatus is sent as a response to user
+type UserStatus struct {
+	UserStatusList []UserStatusEntry `json:"list"`
+	Price          float64           `json:"price"`
 }
 
 // EncryptUser encrypts user info into hash using SHA256
@@ -42,4 +61,48 @@ func EncryptUser(name string, phone string) ([]byte, error) {
 	}
 
 	return h.Sum(nil), nil
+}
+
+// GetTotalPrice will return the status of user
+func (u User) GetStatus(address []byte) (UserStatus, error) {
+	var user *User = nil
+
+	for _, u := range UserData.Data {
+		if bytes.Equal(user.Address, address) {
+			user = &u
+		}
+	}
+
+	// no matching user
+	if user == nil {
+		return UserStatus{}, ErrNoMatchingUser
+	}
+
+	var statlist []UserStatusEntry
+	var price float64 = 0.0
+
+	for dAddress, used := range user.Usage {
+		h, err := hex.DecodeString(dAddress)
+		device, err := DeviceData.Find(h)
+
+		if err != nil {
+			return UserStatus{}, err
+		}
+
+		// append usage information of a device
+		statlist = append(statlist, UserStatusEntry{
+			Dtype: device.dtype,
+			Did:   device.did,
+			Rate:  device.rate,
+			Used:  used,
+		})
+
+		// accumulate user fee
+		price += used * device.rate
+	}
+
+	return UserStatus{
+		UserStatusList: statlist,
+		Price:          price,
+	}, nil
 }
